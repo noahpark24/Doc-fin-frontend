cf6a6cdbaffc9fc8aba17c7ddb376a2360618faf
ivan correa <ivan.correa24@yahoo.com>
2024-03-02 22:25:48 -0300

    feat:movements-cards-updated

--- src/Components/MovementSectionComponents/IncomeMovementCard.tsx
+++ src/Components/MovementSectionComponents/IncomeMovementCard.tsx
@@ -3,41 +3,41 @@ import { SafeAreaView, ScrollView, Text, View } from 'react-native';
 import tw from 'twrnc';
 //Icons
 import { AntDesign } from '@expo/vector-icons';
+//States
+import Movements from '../../store/Movements';
 
 const IncomeMovementCard = () => {
-  let income = {
-    id: 1,
-    name: 'regalo',
-    amount: 500,
-    date: '10/02/24',
-  };
-
   return (
     <>
       {/*Income Movement Card */}
-      <View style={tw.style('flex items-center justify-center mt-2')}>
+      {Movements.incomes.map((income, index) => (
         <View
-          style={tw.style(
-            'bg-green-300 rounded-md w-78 h-12 my-auto flex flex-row justify-between  '
-          )}
+          key={index}
+          style={tw.style('flex items-center justify-center mt-2')}
         >
-          <AntDesign
-            style={tw.style('ml-2 mt-2')}
-            name="pluscircle"
-            size={30}
-            color="green"
-          />
-          <View>
+          <View
+            style={tw.style(
+              'bg-green-300 rounded-md w-78 h-12 my-auto flex flex-row justify-between  '
+            )}
+          >
+            <AntDesign
+              style={tw.style('ml-2 mt-2')}
+              name="pluscircle"
+              size={30}
+              color="green"
+            />
+            <View>
+              <Text
+                style={tw.style(' text-base text-center')}
+              >{`${income.name.substring(0, 18)}`}</Text>
+              <Text style={tw.style(' text-center ')}>{`${income.date}`}</Text>
+            </View>
             <Text
-              style={tw.style(' text-base text-center')}
-            >{`${income.name.substring(0, 16)}`}</Text>
-            <Text style={tw.style(' text-center ')}>{`${income.date}`}</Text>
+              style={tw.style('text-green-800 text-lg mr-2 mt-1')}
+            >{`+ ${income.amount}`}</Text>
           </View>
-          <Text
-            style={tw.style('text-green-800 text-lg mr-2 mt-1')}
-          >{`+ ${income.amount}`}</Text>
         </View>
-      </View>
+      ))}
     </>
   );
 };
